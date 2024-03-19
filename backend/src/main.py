cb8cdf41e481248fcb7593e255c8e762c689ccb7
Anastasia7Si <120373288+Anastasia7Si@users.noreply.github.com>
2024-03-19 15:01:16 +0300

    add front

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,7 +3,16 @@ from .api_v1.applications import router
 from . import models
 from .database import engine
 
-app = FastAPI()
+app = FastAPI(
+    title='HRSpace',
+    summary='A builder for finding recruiters',
+    version='0.0.1',
+    license_info={
+        'name': 'HRSpace',
+        'url': 'https://HRSpace.ru/',
+    }
+)
+
 app.include_router(router.router)
 models.Base.metadata.create_all(bind=engine)
 
